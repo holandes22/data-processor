aaff793934e3615dc5f7774339fa9d73d2a35e4a
Pablo Klijnjan <pablo@factordata.com>
2015-09-13 18:21:57 +0300

    Queueing

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,37 @@
+#[macro_use]
+extern crate log;
 extern crate glob;
+extern crate amqp;
 extern crate hyper;
+extern crate env_logger;
 extern crate rustc_serialize;
 
+use std::env;
+use std::thread;
 use std::fs::File;
 use std::error::Error;
 use std::io::BufRead;
 use std::io::BufReader;
 use std::result::Result;
-use std::env;
 
 use glob::glob;
-use rustc_serialize::json;
 use hyper::Client;
 use hyper::status::StatusCode;
+use rustc_serialize::json;
+use amqp::protocol;
+use amqp::table;
+use amqp::basic::Basic;
+use amqp::session::Session;
+use amqp::channel::{Channel, ConsumerCallBackFn};
 
 
-fn main() {
+fn process(channel: &mut Channel, deliver: protocol::basic::Deliver, headers: protocol::basic::BasicProperties, body: Vec<u8>){
+    let glob_path = String::from_utf8_lossy(&body);
+    debug!("Got path: {:?}", glob_path);
+    channel.basic_ack(deliver.delivery_tag, false);
+
     let mut payload = "".to_string();
-    for path in glob(env::args[0]).unwrap().filter_map(Result::ok) {
+    for path in glob(&glob_path).unwrap().filter_map(Result::ok) {
         let file = match File::open(&path) {
             Ok(file) => file,
             Err(why) => panic!("couldn't open {}: {}", path.display(),
@@ -36,7 +50,7 @@ fn main() {
 
                 },
                 Err(e) => {
-                    println!("error parsing line: {:?}", e);
+                    error!("error parsing line: {:?}", e);
                 }
             }
         }
@@ -47,4 +61,29 @@ fn main() {
         .send()
         .unwrap();
     assert_eq!(res.status, StatusCode::NoContent);
+}
+
+
+fn main() {
+    env_logger::init().unwrap();
+    let amqp_url = "amqp://guest:guest@127.0.0.1//";
+    let mut session = match Session::open_url(amqp_url) {
+        Ok(session) => session,
+        Err(error) => panic!("Can't create session: {:?}", error)
+    };
+    let mut channel = session.open_channel(1).ok().expect("Can't open channel");
+
+    channel.basic_consume(
+        process as ConsumerCallBackFn, "processor", "", false, false, false, false, table::new());
+    channel.start_consuming();
+
+    let consumers_thread = thread::spawn(move || {
+        channel.start_consuming();
+        channel
+    });
+
+    // There is currently no way to stop the consumers, so we infinitely join thread.
+    let mut channel = consumers_thread.join().ok().expect("Can't get channel from consumer thread");
+    channel.close(200, "Closing channel".to_string());
+    session.close(200, "Closing session".to_string());
 }
